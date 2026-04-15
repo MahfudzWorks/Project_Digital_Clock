6c9363b75b7c2775f3dbb195c6454091112ca3e7
Mahfudz Alfanani Syaviqi <mahfudzworksss@gmail.com>
2026-04-15 06:25:55 -0700

    deploy root

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/", // ⬅️ ini kunci
   plugins: [tailwindcss(), react()],
 });
