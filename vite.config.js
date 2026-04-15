1b5a2896814a3fa9dcd5dfac88e1996b61a81aa6
Mahfudz Alfanani Syaviqi <mahfudzworksss@gmail.com>
2026-04-15 06:58:39 -0700

    deploy vite

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/", // ⬅️ ini kunci
+  base: "/Project_Digital_Clock/",
   plugins: [tailwindcss(), react()],
 });
